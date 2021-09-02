ff382915914ae2411b367b3251abd7e9c20789a1
ceelogre <sam.nishimwe@yahoo.com>
2021-09-02 14:56:39 +0200

    feat: parse and submit data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
+import app from './firebase.js'
 // Retrieve data from the form and send it to firebase
-const createTranslationTicket = (event) => {
+const parseForm = (event) => {
   event.preventDefault()
 let userForm = document.forms[0]
 let username = userForm.elements['username'].value
 let email = userForm.elements['email'].value
 let text = userForm.elements['text'].value
 let translateTo = userForm.elements['language'].value
-  let userData = {
+  let ticket = {
     username,
     email,
     text,
     translateTo
   }
-  console.info(userData)
+  createTranslationTicket(ticket)
 }
+const  createTranslationTicket = (ticket) => {
+  let db = app.firestore()
+  db.collection('tickets').add(ticket)
+    .then(
+      docRef => console.info(docRef)
+    )
+    .catch(
+      error => console.error(error)
+    )
+}
+export default parseForm
