05a192c4044b6818e5f7bd22bc3c6a252e52d86f
ceelogre <sam.nishimwe@yahoo.com>
2021-09-08 12:21:42 +0200

    add webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    port: 3001,
+    hot: true,
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './index.html',
+      filename: './index.html',
+      favicon: './assets/favicon.ico'
+    })
+  ],
+  module: {
+    rules: [
+      // add css loader
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
+    clean: true
   },
 };
